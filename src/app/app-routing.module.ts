8c9729a3d390073a092568780c818f28354f3e6c
Dhananjay Mane <dhananjay.mane@xoriant.com>
2019-05-29 16:06:18 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './containers/home/home.component';
+import { FormComponent } from './containers/form/form.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'forms', component: FormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { HomeComponent } from './containers/home/home.component';
+import { FormComponent } from './containers/form/form.component';
+import { UsersComponent } from './containers/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FormComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
